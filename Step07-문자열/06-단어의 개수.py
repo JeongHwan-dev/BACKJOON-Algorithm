979817f321bfb239b1c50eb7f5b3baf5725338b5
Park Jeong Hwan <jeonghwan.dev@gmail.com>
2021-04-14 23:41:29 +0900

    04.14. study

---
+++ Step07-문자열/06-단어의 개수.py
@@ -0,0 +1,18 @@
+"""
+예제 입력 1
+The Curious Case of Benjamin Button
+예제 출력 1
+6
+예제 입력 2
+ Mazatneunde Wae Teullyeoyo
+예제 출력 2
+3
+예제 입력 3
+Teullinika Teullyeotzi
+예제 출력 3
+2
+"""
+
+words = input().split()
+
+print(len(words))
